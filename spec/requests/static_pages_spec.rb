558e6a1b01a41a96c8171a2c967c41641752c52d
KAZU <kazu@kazu-X101CH.(none)>
2014-04-28 18:44:49 +0900

    Refactoring spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,54 +4,37 @@ describe "Static pages" do
 
   let(:base_title) { "Ruby on Rails Tutorial Sample App"}
 
+  subject { page }
+
   describe "Home page:ホームページに対するテスト" do
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home' #ブラウザアクセス
-      expect(page).to have_content('Sample App')
-    end
-    it "should have the right title" do
-      visit '/static_pages/home'
-      expect(page).to have_title("#{base_title}")
-    end
-    it "should not have a custome page title" do
-      visit '/static_pages/home'
-      expect(page).not_to have_title('| Home')
-    end
+    before { visit root_path }
+    it { should have_content('Sample App') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }
   end
 
   describe "Help page:ヘルプページに対するテスト" do
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
-    it "should have the right title" do
-      visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
-      expect(page).to have_title("#{base_title} | Help")
-    end
- 
+    before { visit help_path }
+    it { should have_content('Help') }
+    
+    #色々な書き方
+    it {
+      should have_title("Ruby on Rails Tutorial Sample App | Help")
+      should have_title("#{base_title} | Help")
+      should have_title(full_title('Help'))
+    }
   end
 
   describe "About page:アバウトページに対するテスト" do
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About Us')
-    end
-    it "should have the right title" do
-      visit '/static_pages/about'
-      expect(page).to have_title("#{base_title} | About")
-    end
+    before { visit about_path }
+    it { should have_content('About Us') }
+    it { should have_title(full_title('About')) }
   end
 
   describe "Contact page:コンタクトに対するテスト" do
-    it "should have the content 'contact'" do
-      visit '/static_pages/contact' #ブラウザアクセス
-      expect(page).to have_content('Contact')
-    end
-    it "should have the right title" do
-      visit '/static_pages/contact'
-      expect(page).to have_title("#{base_title} | Contact")
-    end
+    before { visit contact_path }
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
   end
 
 end
